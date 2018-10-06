975a0bc61ea61a0073aaf3c9543febb1b591f0b5
Jordan Knight <jordan_r_knight@hotmail.com>
2018-10-06 17:48:17 -0400

    Procedural map generation
    Added basic procedural generation to the map maker script.

--- Maze Game/Assets/Scripts/MapMaker.cs
+++ Maze Game/Assets/Scripts/MapMaker.cs
@@ -8,23 +8,97 @@ public class MapMaker : MonoBehaviour {
 	public int tileHeight = 4;
 	public float xOffset = 0;
 	public float zOffset = 0;
+    public int mapSize = 50;
+    public bool loadFromTxt;
     public GameObject level;
     public GameObject[] prefabs;
 	public string characters;
 	public TextAsset[] asciiMap;
 
-	private int mapWidth;    
+
+    private int mapWidth;    
 	private string map;
     private NavMeshSurface surface;
+    private int[,] mapGrid;
+    private int xPos, yPos, dir;
+    private int steps;
+    private string proceduralMap = "";
+    private bool hasSpawn = false;
+    // private float wave;
 	
 	void Start() {
-        GenerateMap();
+        GenerateMap(loadFromTxt);
     }
 
-    private void GenerateMap() {
+    private void GenerateMap(bool loadFromTxt) {
         level = Instantiate(level);
         level.name = "Level";
-        for (int i = 0; i < asciiMap.Length; i++) LoadMap(asciiMap[i], i);
+        if (loadFromTxt) {
+            for (int i = 0; i < asciiMap.Length; i++) LoadMap(asciiMap[i], i);
+        } else {
+            // procedurally generate map
+            mapGrid = new int[mapSize, mapSize];
+            // set map digger start position to middle of map
+            xPos = mapSize / 2;
+            yPos = mapSize / 2;
+            steps = mapSize * mapSize / 4;
+            for (int i = 0; i < steps; i++) {
+                //move digger
+                //wave = Mathf.Sin(steps/mapSize) * 0.5f + 1f;
+                if (Random.Range(0f, 1f) > 0.75f) dir = Random.Range(0, 4);
+                switch (dir) {
+                    case 0:
+                        xPos++;
+                        break;
+                    case 1:
+                        yPos++;
+                        break;
+                    case 2:
+                        xPos--;
+                        break;
+                    case 3:
+                        yPos--;
+                        break;
+                }
+                // prevent digger from out of bounds
+                xPos = Mathf.Clamp(xPos, 1, mapSize - 2);
+                yPos = Mathf.Clamp(yPos, 1, mapSize - 2);
+                mapGrid[xPos, yPos] = 1; // set current position to floor
+            }
+            for (int y = 1; y < mapSize-1; y++) {
+                for (int x = 1; x < mapSize-1; x++) {
+                    if(mapGrid[x,y] == 1) {
+                        if (mapGrid[x + 1, y] == 0) mapGrid[x + 1, y] = 2;
+                        if (mapGrid[x - 1, y] == 0) mapGrid[x - 1, y] = 2;
+                        if (mapGrid[x, y + 1] == 0) mapGrid[x, y + 1] = 2;
+                        if (mapGrid[x, y - 1] == 0) mapGrid[x, y - 1] = 2;
+                    }
+                }
+            }
+                    for (int y = 0; y < mapSize; y++) {
+                for (int x = 0; x < mapSize; x++) {
+                    switch (mapGrid[x, y]) {
+                        case 0:
+                            proceduralMap += "H";
+                            break;
+                        case 1:
+                            if (!hasSpawn) {
+                                proceduralMap += "C";
+                                hasSpawn = true;
+                            } else {
+                                proceduralMap += "O";
+                            }
+                            break;
+                        case 2:
+                            proceduralMap += "X";
+                            break;
+                    }
+                }
+                proceduralMap += "\n";
+            }
+            mapWidth = mapSize + 1;
+            ParseMap(proceduralMap, 1);
+        }
         surface = level.GetComponent<NavMeshSurface>();
         surface.BuildNavMesh();
     }
